59a3a9026ab358f35fc43758c95891a081007917
Tajeshwar Khara <tkhara@gmail.com>
2016-10-18 17:32:10 +0530

    Area of Triangle
    Using the measurements of the sides.

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcArea();
+        }
+
+        static void CalcArea()
+        {
+            double a, b, c, p, area;
+            Console.WriteLine("Please enter the measurements of the three sides of a triangle");
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            p = (a + b + c) / 2;
+
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine("The area of the triangle is {0}", area);
+
+
+        }
+    }
+}
